77c52af17cd37733428aba620cae4ee7be3f583d
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2025-10-05 12:22:45 +0100

    Add Constants

---
+++ HppFiles/constants.hpp
@@ -0,0 +1,11 @@
+#ifndef CONSTANTS_HPP
+#define CONSTANTS_HPP
+
+#define RESET "\033[0m"
+#define RED "\033[4;31m"
+#define GREEN "\033[1;32m"
+#define YELLOW "\033[1;33m"
+#define CYAN "\033[1;36m"
+#define ORANGE "\033[38;5;208m"
+
+#endif
